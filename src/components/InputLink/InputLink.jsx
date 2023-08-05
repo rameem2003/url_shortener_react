cddcbcd264be01094cd1db7b305ce1bb9d12d972
rameem2003 <rameem2019@gmail.com>
2023-08-05 21:47:20 +0600

    update

--- src/components/InputLink/InputLink.jsx
+++ src/components/InputLink/InputLink.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react'
-import './inputlink.css';
+import React, { useState } from "react";
+import "./inputlink.css";
 
-const InputLink = ({setValue}) => {
-
+const InputLink = ({ setValue }) => {
   const [getValue, setGetvalue] = useState("");
 
   const handleClick = () => {
     setValue(getValue);
-    setGetvalue('')
-  }
+    setGetvalue("");
+  };
 
   return (
-    <div className='inputsection'>
-      <h1>URL <span>Shortener <sup style={{fontSize : "22px"}}>Beta</sup></span></h1>
+    <div className="inputsection">
+      <h1>
+        URL{" "}
+        <span>
+          Shortener <sup style={{ fontSize: "22px" }}>Beta</sup>
+        </span>
+      </h1>
 
       <div className="input">
-        <input type="text" name="" id="" placeholder='Enter Your Link' value={getValue} onChange={(e) => {setGetvalue(e.target.value)}}/>
-        <button type="button" onClick={handleClick}>Short</button>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Enter Your Link"
+          value={getValue}
+          onChange={(e) => {
+            setGetvalue(e.target.value);
+          }}
+        />
+        <button type="button" onClick={handleClick}>
+          Short
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InputLink
+export default InputLink;
